6bcab3e66cbdb4192b19de979e2d42836c7f4d72
ajauniskis <jauniskis.a@gmail.com>
2022-10-26 23:10:08 +0300

    adding mongo client

---
+++ app/db/client.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from fastapi.encoders import jsonable_encoder
+
+from app.db.database import get_client
+from app.db.models.joke import JokeModel
+
+
+class MongoClient:
+    def __init__(self) -> None:
+        self.database_client = get_client()
+
+    async def read_all(
+        self, collection_name: str, page_size: int = 100
+    ) -> list[JokeModel]:
+        collection = await self.database_client.get_collection(collection_name)
+
+        response = await collection.find().to_list(page_size)
+        return [JokeModel(**resp) for resp in response]
+
+    async def read_random(self, collection_name) -> JokeModel:
+        collection = await self.database_client.get_collection(collection_name)
+
+        cur = collection.aggregate([{"$sample": {"size": 1}}])
+        response = await cur.next()
+
+        return JokeModel(**response)
+
+    async def write(self, record: JokeModel, collection_name: str) -> JokeModel:
+        collection = await self.database_client.get_collection(collection_name)
+
+        record.inserted_at = datetime.now()
+        record = jsonable_encoder(record)
+        new_record = await collection.insert_one(record)
+        created_record = await collection.find_one({"_id": new_record.inserted_id})
+        created_model = JokeModel(**created_record)
+
+        return created_model
+
+
+client = MongoClient()

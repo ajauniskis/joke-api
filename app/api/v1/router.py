9c188df952722c4d81ffce524c358fd16982c80b
ajauniskis <jauniskis.a@gmail.com>
2022-10-31 22:32:35 +0200

    adding joke endpoint

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints import info
+from app.api.v1.endpoints import joke
 
 api_v1_router = APIRouter(prefix="/api/v1")
 api_v1_router.include_router(info.router)
+api_v1_router.include_router(joke.router)

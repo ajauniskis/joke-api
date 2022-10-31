8c1c94a94336091505c03025b8f4cf664e98de45
ajauniskis <jauniskis.a@gmail.com>
2022-11-01 01:24:29 +0200

    async setup test collection

--- tests/api/v1/endpoints/test_joke.py
+++ tests/api/v1/endpoints/test_joke.py
@@ -1,28 +1,40 @@
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase
 
+import pytest
 from fastapi.testclient import TestClient
 
 from app.api.v1.schemas.joke import PostJokeRequest
 from app.core.app import app
+from app.db.mongodb import get_client
 
 
-class TestJoke(TestCase):
-    def setUp(self) -> None:
+@pytest.mark.asyncio
+class TestJoke(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        self.test_collection_name = "test_collection"
+        self.mongo_client = get_client()
+        await self.mongo_client._create_collection(self.test_collection_name)
         self.client = TestClient(app)
         self.post_joke_request = PostJokeRequest(
-            category="dev",
+            category=self.test_collection_name,
             question="question",
             punchline="punchline",
         )
+
+    async def asyncTearDown(self) -> None:
+        test_collection = await self.mongo_client.get_collection(
+            self.test_collection_name
+        )
+        await self.mongo_client.drop_collection(test_collection)
 
     def test_post_joke__returns_201(self):
         response = self.client.post(
             "/api/v1/joke/",
             json=self.post_joke_request.dict(),
-        ).status_code
+        )
 
         self.assertEqual(
-            response,
+            response.status_code,
             201,
         )
 
@@ -30,12 +42,12 @@ class TestJoke(TestCase):
         response = self.client.post(
             "/api/v1/joke/",
             json=self.post_joke_request.dict(),
-        ).json()
+        )
 
         expected = self.post_joke_request.to_response()
 
         self.assertEqual(
-            response,
+            response.json(),
             expected,
         )
 

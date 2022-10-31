fbda880548f57a146306dcca546a8b9ea84b3e7e
ajauniskis <jauniskis.a@gmail.com>
2022-11-01 00:39:15 +0200

    sorting imports

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import info
-from app.api.v1.endpoints import joke
+from app.api.v1.endpoints import info, joke
 
 api_v1_router = APIRouter(prefix="/api/v1")
 api_v1_router.include_router(info.router)

3231febb03c94dfa92fe9fe2dcb02e17365cc6d4
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2022-12-17 20:39:52 +0200

    update test for new props

--- tests/api/v1/endpoints/test_info.py
+++ tests/api/v1/endpoints/test_info.py
@@ -23,10 +23,16 @@ class TestInfo(TestCase):
         config = settings.ProjectConfigParser()
         expected = {
             "title": settings.app_name,
-            "description": config.get_project_description(),
-            "version": config.get_project_version(),
-            "contacts": config.get_project_contacts(),
+            "description": config.description,
+            "version": config.version,
+            "contacts": {
+                "url": str(config.contacts["url"]),
+            },
             "categories": settings.categories,
+            "license": {
+                "name": config.license_name,
+                "url": str(config.license_url),
+            },
         }
         actual = self.client.get("/api/v1/info").json()
 

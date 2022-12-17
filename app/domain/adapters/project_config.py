d1cf548a3cd2341af38965e5fd8c93165108e483
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2022-12-17 20:29:44 +0200

    adding ProjectConfigAdapter

---
+++ app/domain/adapters/project_config.py
@@ -0,0 +1,28 @@
+from app.core.logger import logger
+from app.core.settings import get_settings
+from app.domain import exceptions
+from app.domain.adapters.base import BaseAdapter
+from app.domain.models.project_config import Contacts, License, ProjectConfig
+
+
+class ProjectConfigAdapter(BaseAdapter):
+    project_config = get_settings().ProjectConfigParser()
+
+    async def get(self) -> ProjectConfig:
+        return ProjectConfig(
+            version=self.project_config.version,
+            description=self.project_config.description,
+            contacts=Contacts(
+                url=self.project_config.contacts["url"],
+            ),
+            license=License(
+                name=self.project_config.license_name,
+                url=self.project_config.license_url,
+            ),
+        )
+
+    async def post(self) -> None:
+        logger.error(f"Post method is not supported in {self.__class__.__name__}")
+        raise exceptions.NotSupportedException(
+            f"Post method is not supported in {self.__class__.__name__}"
+        )

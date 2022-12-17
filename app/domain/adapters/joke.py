f64ba77de4bc120c2baf373e822bd33006a653da
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2022-12-17 20:29:33 +0200

    adding JokeAdapter

---
+++ app/domain/adapters/joke.py
@@ -0,0 +1,18 @@
+from app.api.v1.schemas.joke import PostJokeRequest, PostJokeRespone
+from app.db.database import client as database
+from app.domain.adapters.base import BaseAdapter
+
+
+class JokeAdapter(BaseAdapter):
+    joke_repository = NotImplemented
+
+    async def get(self):
+        raise NotImplementedError
+
+    @classmethod
+    async def post(cls, request: PostJokeRequest) -> PostJokeRespone:
+        database_model = await request.to_database_model()
+        await database.write(database_model, request.category)
+
+        response = request.to_response()
+        return response

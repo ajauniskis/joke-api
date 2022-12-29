7ee17388fb07ce067e7aa6b14d78227f5759c76c
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2022-12-30 00:52:39 +0200

    adding joke repository override

--- app/overrides/joke_repository.py
+++ app/overrides/joke_repository.py
@@ -3,11 +3,11 @@ from app.repositories import JokeRepository
 
 
 class JokeRepositoryOverride(JokeRepository):
-    async def _add(self, joke: JokeModel) -> JokeModel:
+    async def add(self, joke: JokeModel) -> JokeModel:
         return joke
 
-    async def _get_all(self):
+    async def get_all(self):
         pass
 
-    async def _get_random(self):
+    async def get_random(self):
         pass

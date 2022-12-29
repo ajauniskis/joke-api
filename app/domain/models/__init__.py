5d81a4a433847ff8047b6c5ea37f97d2c4a2da87
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2022-12-29 23:56:54 +0200

    init domain models

---
+++ app/domain/models/__init__.py
@@ -0,0 +1,9 @@
+from app.domain.models.joke import JokeModel
+from app.domain.models.project_config import ProjectConfig
+from app.domain.models.settings import Settings
+
+__all__ = [
+    "JokeModel",
+    "ProjectConfig",
+    "Settings",
+]
